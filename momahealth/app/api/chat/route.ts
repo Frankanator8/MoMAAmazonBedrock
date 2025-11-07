fe3c03b8659a29db3b65d4fa151a98a767761135
Christopher Cao <84947701+derp000@users.noreply.github.com>
2025-11-07 10:42:16 -0500

    Add aws endpoint

--- momahealth/app/api/chat/route.ts
+++ momahealth/app/api/chat/route.ts
@@ -1,12 +1,13 @@
-import { openai } from "@ai-sdk/openai";
-import { streamText, UIMessage, convertToModelMessages } from "ai";
+import { bedrock } from "@ai-sdk/amazon-bedrock";
+import { convertToModelMessages, streamText } from "ai";
+
+export const maxDuration = 30;
 
 export async function POST(req: Request) {
-  const { messages }: { messages: UIMessage[] } = await req.json();
+  const { messages } = await req.json();
   const result = streamText({
-    model: openai("gpt-5-nano"),
+    model: bedrock("anthropic.claude-3-5-sonnet-20240620-v1:0"),
     messages: convertToModelMessages(messages),
   });
-
   return result.toUIMessageStreamResponse();
 }
